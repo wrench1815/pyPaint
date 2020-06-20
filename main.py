0ff2be6a18b8fb99645068885b58422bea2e9fe2
Hardeep Kumar <hardeepkumar1815@gmail.com>
2020-06-20 18:25:02 +0530

    add Canvas

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from tkinter import *
 
 root = Tk()
 root.title("wrench1815's Paint Program")
-root.geometry("800x800")
+root.geometry("800x650")
+
+wt = 600
+ht = 400
+
+root_canvas = Canvas(root, width=wt, height=ht, bg="white")
+root_canvas.pack(pady=20)
 
 root.mainloop()
