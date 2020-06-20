8ac6c08a5c237124c8f9b6de76c7a83afe907d11
Hardeep Kumar <hardeepkumar1815@gmail.com>
2020-06-20 17:18:10 +0530

    created a windows

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ Author : Hardeep Kumar
 Created On : Sat Jun 20 2020
 File : main.py
 """
-import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("wrench1815's Paint Program")
+root.geometry("800x800")
+
+root.mainloop()
