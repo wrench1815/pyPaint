0f069ffafb62ddd4ce890b58c0f1db08353a9271
wrench1815 <hardeepkumar1815@gmail.com>
2020-06-23 00:01:13 +0530

    update main

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ Author : Hardeep Kumar
 Created On : Sat Jun 20 2020
 File : main.py
 """
-import tkinter.ttk as ttk
 from tkinter import *
+import tkinter.ttk as ttk
 
 root = Tk()
 root.title("pyPaint")
-root.geometry("800x650")
+root.geometry("800x800")
 
 
 def draw(e):
     # Brush Parameters
     brush_colour = 'green'
-    brush_size = 20
+    brush_size = int(brush_size_slider.get())
     # Types Available:- BUTT, ROUND, PROJECTING
-    brush_type = ROUND
+    brush_type_canvas = ROUND
 
     # Starting Axis
     x1, y1 = e.x - 1, e.y - 1
@@ -29,20 +29,23 @@ def draw(e):
                             y2,
                             fill=brush_colour,
                             width=brush_size,
-                            capstyle=brush_type,
+                            capstyle=brush_type_canvas,
                             smooth=True)
 
 
+# function changes brush width using Slider values
 def change_brush_size(pos_return):
     brush_size_slider_label.config(text=int(brush_size_slider.get()))
 
 
+# Dimensions of Canvas
 wt = 600
 ht = 400
 
 root_canvas = Canvas(root, width=wt, height=ht, bg="white")
 root_canvas.pack(pady=20)
 
+# Bind draw action to left click + Drag
 root_canvas.bind('<B1-Motion>', draw)
 
 # Brush Options Frame
@@ -66,4 +69,31 @@ brush_size_slider.pack(pady=10, padx=10)
 brush_size_slider_label = Label(brush_size_frame, text=brush_size_slider.get())
 brush_size_slider_label.pack(pady=5)
 
+# Brush Type
+brush_type_frame = LabelFrame(brush_options_frame,
+                              text="Brush Type",
+                              height=400)
+brush_type_frame.grid(row=0, column=1, padx=50)
+
+brush_type = StringVar()
+brush_type.set("round")
+
+# Brush Type Radios
+brush_type_radio1 = Radiobutton(brush_type_frame,
+                                text="Round",
+                                variable=brush_type,
+                                value="round")
+brush_type_radio2 = Radiobutton(brush_type_frame,
+                                text="Slash",
+                                variable=brush_type,
+                                value="butt")
+brush_type_radio3 = Radiobutton(brush_type_frame,
+                                text="Diamond",
+                                variable=brush_type,
+                                value="projecting")
+brush_type_radio1.pack(anchor=W)
+brush_type_radio2.pack(anchor=W)
+brush_type_radio3.pack(anchor=W)
+
+# Repeat the actions
 root.mainloop()
